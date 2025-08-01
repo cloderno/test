8b454ca5369de7cf0790a3c2405f21bed7be341a
cloderno <cloderno@gmail.com>
2025-08-01 10:49:56 +0500

    append, когда файл новый не берет newline

--- src/FileManager.java
+++ src/FileManager.java
@@ -95,7 +95,7 @@ public class FileManager {
                 : new OpenOption[] { StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING };
 
         try (BufferedWriter writer = Files.newBufferedWriter(outputPath, DEFAULT_CHARSET, options)) {
-            if (argsProcessor.isAppendOption()) {
+            if (argsProcessor.isAppendOption() && Files.exists(outputPath) && Files.size(outputPath) > 0) {
                 writer.newLine();
             }
             writer.write(content, 0, content.length());
